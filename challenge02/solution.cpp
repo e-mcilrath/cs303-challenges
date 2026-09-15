d73b61ae1677b41140a680e6c79a99e835e551c8
Eric McIlrath <edmcilrath@gmail.com>
2026-09-14 20:28:18 -0400

    challenge 2 and 3

--- challenge02/solution.cpp
+++ challenge02/solution.cpp
@@ -1,10 +1,13 @@
 // Challenge 02: Closest Numbers
-// Name:  Eric McIlrath
+// Name:  Eric McIlrath 
+// NetID: emcilrat
 
 // Brief description:
 
-// This code solves yyyy based on the following idea/implementation...
-
+/* 
+This code finds the pair of integers in a list 
+that have the least absolute numerical distance between them
+*/
 #include <algorithm>
 #include <climits>
 #include <cstdlib>
@@ -19,31 +22,31 @@ int main(int argc, char *argv[]) {
 
     int amt;
 
-    while (cin >> amt) {
-        vector<int> nums;
+    while (cin >> amt) { 
+        vector<int> nums; // the main vector of ints
 
-        for (int i = 0; i < amt; i++) {
+        for (int i = 0; i < amt; i++) { // ingest the numbers
             int num;
             cin >> num;
             nums.push_back(num);
         }
 
-        sort(nums.begin(), nums.end());
+        sort(nums.begin(), nums.end()); // sort 'em
 
-        int diff = INT_MAX;
+        int diff = INT_MAX; // this must be int max so that the first time this code is run, the diff will be the smallest
 
-        for (int i = 0; i < amt - 1; i++) {
+        for (int i = 0; i < amt - 1; i++) { // go through the list calculating diff, 2 adjacent numbers at a time
             int tmp_diff = nums[i + 1] - nums[i];
             if (tmp_diff < diff) {
                 diff = tmp_diff;
             }
         }
 
-        bool first = true;
-        for (int i = 0; i < amt - 1; i++) {
+        bool first = true; // for proper spacing
+        for (int i = 0; i < amt - 1; i++) { // print pairs if diff == smallest diff
             if (nums[i + 1] - nums[i] == diff) {
                 if (!first) {
-                    cout << " ";
+                    cout << " "; // space
                 }
                 cout << nums[i] << " " << nums[i + 1];
                 first = false;
--- challenge03/solution.cpp
+++ challenge03/solution.cpp
@@ -1,6 +1,52 @@
-// Challenge 03: Palindrome Permutation
+// Challenge 03
+// Eric McIlrath
+// emcilrat
+
+/*
+This code reads in a string and determines if it is a 
+pallindrome by counting the number each character appears in
+the string. If there is a character that appears an odd number
+of time which is greater than 1, not a pallindrome.
+*/
+
+#include <iostream>
+#include <map>
+#include <string>
+
+using namespace std;
+
+// Main Execution
 
 int main(int argc, char *argv[]) {
-  return (0);
-}
 
+    string line;
+
+    while (getline(cin, line)) {
+        map<char, int> letter_count; // how many times each letter appears
+
+        for (size_t i = 0; i < line.size(); i++) { // ingest letters with count
+            char c = line[i];
+            if (c >= 'A' && c <= 'Z') { // make lowrcase
+                c = c - 'A' + 'a';
+            }
+            if (c >= 'a' && c <= 'z') { // anything else is punctuation or space
+                letter_count[c]++;
+            }
+        }
+
+        int odds = 0; // letters appearing odd number times
+        for (map<char, int>::iterator it = letter_count.begin(); it != letter_count.end(); ++it) {
+            if (it->second % 2 != 0) { // check if odd
+                odds++;
+            }
+        }
+
+        cout << "\"" << line << "\" is "; 
+        if (odds > 1) { // if there is more than one odd letter, not a pallindrome 
+            cout << "not ";
+        }
+        cout << "a palindrome permutation" << endl;
+    }
+
+    return 0;
+}
